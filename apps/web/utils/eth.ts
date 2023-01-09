574209a3002a4f4fdd3d658bac46e35d1cce1eb9
theNvN <thenvnsahu@gmail.com>
2023-01-10 03:31:56 +0530

    refactor: rename

--- apps/web/utils/eth.ts
+++ apps/web/utils/eth.ts
@@ -73,6 +73,6 @@ export const isValidAddress = (address?: string) => {
   return utils.isAddress(address);
 };
 
-export const currentBlockTimestamp = async (provider: ethers.providers.BaseProvider) => {
+export const latestBlockTimestamp = async (provider: ethers.providers.BaseProvider) => {
   return provider.getBlock('latest').then((block) => block.timestamp);
 };
