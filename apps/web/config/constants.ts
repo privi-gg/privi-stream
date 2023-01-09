51177c24650d09feaa1a44bbbecb172eecd0d26e
theNvN <thenvnsahu@gmail.com>
2023-01-10 03:53:22 +0530

    add app routes

--- apps/web/config/constants.ts
+++ apps/web/config/constants.ts
@@ -3,3 +3,10 @@ export const APP_NAME = 'Privi Stream';
 export const SIGN_MESSAGE = 'Sign this message to generate a private key';
 
 export const TREE_HEIGHT = 20;
+
+export const ROUTES = {
+  HOME: '/',
+  CREATE: '/create',
+  WITHDRAW: '/withdraw',
+  REVOKE: '/revoke',
+};
