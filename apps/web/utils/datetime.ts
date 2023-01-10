c4de885defd2729a8232f4653bf7e39ee8226986
theNvN <thenvnsahu@gmail.com>
2023-01-10 18:31:43 +0530

    add util

--- apps/web/utils/datetime.ts
+++ apps/web/utils/datetime.ts
@@ -7,6 +7,8 @@ export function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+export const now = () => dayjs().unix();
+
 export const formatDate = (date: Date | number, format: string = 'DD MMM, hh:mm A') =>
   dayjs(date).format(format);
 
