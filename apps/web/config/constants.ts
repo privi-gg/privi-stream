bb8103287ca30bacfc5f495689344b98eb88335e
theNvN <thenvnsahu@gmail.com>
2023-01-10 18:32:46 +0530

    add constant

--- apps/web/config/constants.ts
+++ apps/web/config/constants.ts
@@ -4,6 +4,8 @@ export const SIGN_MESSAGE = 'Sign this message to generate a private key';
 
 export const TREE_HEIGHT = 20;
 
+export const REVOKE_TIME_DELTA = 5 * 60; // 5 minutes
+
 export const ROUTES = {
   HOME: '/',
   CREATE: '/create',
