45f39a6c1e0a1c86eced7772ca7cb5061836d777
theNvN <thenvnsahu@gmail.com>
2023-01-11 00:41:44 +0530

    add icon

--- apps/web/components/icons/index.tsx
+++ apps/web/components/icons/index.tsx
@@ -1,5 +1,5 @@
 export { BiUserCircle as UserIcon } from 'react-icons/bi';
-export { BiLogOut as LogOutIcon } from 'react-icons/bi';
+// export { BiLogOut as LogOutIcon } from 'react-icons/bi';
 export { BiLogIn as LogInIcon } from 'react-icons/bi';
 export { BiDetail as DetailIcon } from 'react-icons/bi';
 export { BiErrorCircle as ErrorIcon } from 'react-icons/bi';
@@ -9,7 +9,9 @@ export { HiShieldCheck as ShieldCheckIcon } from 'react-icons/hi';
 export { FaKey as KeyIcon } from 'react-icons/fa';
 export { FiArrowRight as ArrowRightIcon } from 'react-icons/fi';
 export { FiChevronRight as ChevronRightIcon } from 'react-icons/fi';
+export { FiChevronDown as ChevronDownIcon } from 'react-icons/fi';
 export { FiDownload as DownloadIcon } from 'react-icons/fi';
 export { FiCopy as CopyIcon } from 'react-icons/fi';
 export { BiTimeFive as TimeIcon } from 'react-icons/bi';
 export { BiCheckCircle as CheckCircleIcon } from 'react-icons/bi';
+export { FiLogOut as LogOutIcon } from 'react-icons/fi';
