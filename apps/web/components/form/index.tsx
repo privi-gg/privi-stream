5f503f6b7d4143d6672cdb9383a7ac8f17c1d14e
theNvN <thenvnsahu@gmail.com>
2023-01-10 18:16:01 +0530

    add date input component

--- apps/web/components/form/index.tsx
+++ apps/web/components/form/index.tsx
@@ -1,2 +1,3 @@
 export { default as FormAddressInput } from './FormAddressInput';
 export { default as FormRateInput } from './FormRateInput';
+export { default as FormDateInput } from './FormDateInput';
